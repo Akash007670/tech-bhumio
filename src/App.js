fafc7fa8c67716df38e1160b1c93cf77c07711d5
Akash kumar <akaysharma894@gmail.com>
2023-06-16 17:26:35 +0530

    Added CSv Reader component to app and added inventory table to pass the data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import "primeicons/primeicons.css";
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+//core
+import "primereact/resources/primereact.min.css";
+import CSVReader from "./components/CSVReader";
+import InventoryTable from "./components/InventoryTable";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [isUploaded, setIsUploaded] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+        style={{
+          display: "flex",
+          border: "2px solid red",
+          marginBottom: "10px",
+          padding: "10px",
+        }}
+      >
+        <h1>Bhumio CSV Data Table</h1>
+      </div>
+      <CSVReader setData={setData} setIsUploaded={setIsUploaded} />
+      {isUploaded && <InventoryTable data={data} setData={setData} />}
     </div>
   );
 }
