3bb6d2591ce1feb77e97a2cc86160ea92b36e9d1
Akash kumar <akaysharma894@gmail.com>
2023-07-08 17:22:18 +0530

    Added json to csv converter and download option feature

--- src/components/InventoryTable.js
+++ src/components/InventoryTable.js
@@ -3,11 +3,15 @@ import InventoryModal from "./InventoryModal";
 import Table from "./Table";
 import { InputText } from "primereact/inputtext";
 import { Button } from "primereact/button";
+import { usePapaParse } from "react-papaparse";
+import { useCSVDownloader } from "react-papaparse";
 
 const InventoryTable = ({ data, setData }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
   const [showModal, setShowModal] = useState(false);
+  const { jsonToCSV } = usePapaParse();
+  const { CSVDownloader } = useCSVDownloader();
 
   useEffect(() => {
     const filteredResult = data.filter((item) => {
@@ -19,6 +23,17 @@ const InventoryTable = ({ data, setData }) => {
     });
     setFilteredData(filteredResult);
   }, [searchQuery, data]);
+
+  const handleJsonToCSV = () => {
+    const jsonData = [...data];
+    let newData = jsonData.map((item) => {
+      delete item.id;
+      delete item.undefined;
+      return item;
+    });
+    let res = jsonToCSV(newData);
+    return res;
+  };
 
   return (
     <div className="inventory-table-wrapper">
@@ -37,7 +52,16 @@ const InventoryTable = ({ data, setData }) => {
           size="small"
           severity="info"
           onClick={() => setShowModal(true)}
+          icon="pi pi-file-edit"
         />
+        <CSVDownloader filename="Inventory" bom={true} data={handleJsonToCSV}>
+          <Button
+            label="Export Data"
+            size="small"
+            severity="info"
+            icon="pi pi-download"
+          />
+        </CSVDownloader>
       </div>
       <InventoryModal
         filteredData={filteredData}
