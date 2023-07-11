c451ae5dd1fcea6e59d4d5f8494aa50472d6aa07
Akash kumar <akaysharma894@gmail.com>
2023-07-12 00:35:10 +0530

    Added sidebar

--- src/components/InventoryTable.js
+++ src/components/InventoryTable.js
@@ -6,12 +6,14 @@ import { Button } from "primereact/button";
 import { usePapaParse } from "react-papaparse";
 import { useCSVDownloader } from "react-papaparse";
 import GraphModal from "./GraphModal";
+import EmbededGraphLink from "./EmbededGraphLink";
 
 const InventoryTable = ({ data, setData }) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [filteredData, setFilteredData] = useState([]);
   const [showModal, setShowModal] = useState(false);
   const [showGraphModal, setShowGraphModal] = useState(false);
+  const [visibleRight, setVisibleRight] = useState(false);
   const { jsonToCSV } = usePapaParse();
   const { CSVDownloader } = useCSVDownloader();
 
@@ -71,8 +73,15 @@ const InventoryTable = ({ data, setData }) => {
           label="Inventory Graph"
           size="small"
           severity="info"
+          icon="pi pi-chart-bar"
           onClick={() => setShowGraphModal(true)}
         />
+        {/* <Button
+          size="small"
+          severity="info"
+          icon="pi pi-code"
+          onClick={() => setVisibleRight(true)}
+        /> */}
       </div>
       <InventoryModal
         filteredData={filteredData}
@@ -85,6 +94,10 @@ const InventoryTable = ({ data, setData }) => {
         showGraphModal={showGraphModal}
         setShowGraphModal={setShowGraphModal}
         data={data}
+      />
+      <EmbededGraphLink
+        visibleRight={visibleRight}
+        setVisibleRight={setVisibleRight}
       />
       <div className="main-table-wrapper">
         {data.length > 0 && (
